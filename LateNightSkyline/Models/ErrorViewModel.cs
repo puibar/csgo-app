6404a81225f370c9947db11e35e12296d5960742
Martin Winberg Olsen <mwolsen123@gmail.com>
2018-07-19 00:12:53 +0200

    Intitial commit

---
+++ LateNightSkyline/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LateNightSkyline.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
